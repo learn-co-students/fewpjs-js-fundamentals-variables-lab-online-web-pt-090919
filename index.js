1ee2bef9de55cdd7b470eb9809e21c2fd5c071ae
Emil Andreasyan <emil.andreasyan@yahoo.com>
2020-03-20 03:37:59 +0000

    Done.

--- index.js
+++ index.js
@@ -1,8 +1,5 @@
 // Code your solution in this file!
-const companyName = 'Scruber';
-companyName
-var mostProfitableNeighborhood = 'Chelsea';
-mostProfitableNeighborhood = 'Upper West Side';
-let companyCeo = 'Susan Smith';
-companyCeo = 'Lauren Hart';
+const companyName = 'Scuber'
+let mostProfitableNeighborhood = 'Chelsea'
+let companyCeo = 'Susan Smith'
 
