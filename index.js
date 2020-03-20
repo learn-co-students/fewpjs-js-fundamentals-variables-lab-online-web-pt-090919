eed71a5570eb67303478d6a4381f6253cb77f69f
Emil Andreasyan <emil.andreasyan@yahoo.com>
2020-03-20 03:36:54 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Code your solution in this file!
+const companyName = 'Scruber';
+companyName
+var mostProfitableNeighborhood = 'Chelsea';
+mostProfitableNeighborhood = 'Upper West Side';
+let companyCeo = 'Susan Smith';
+companyCeo = 'Lauren Hart';
+
